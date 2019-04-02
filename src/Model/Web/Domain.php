9db3de612c4d0bfb19b56f14c36e64c3a5118beb
Marek Gogol <9mike66@gmail.com>
2019-04-02 12:18:07 +0200

    added domain model

---
+++ src/Model/Web/Domain.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Gogol\VpsManager\Model\Web;
+
+use Gogol\Admin\Fields\Group;
+use Gogol\Admin\Models\Model as AdminModel;
+
+class Domain extends AdminModel
+{
+    /*
+     * Model created date, for ordering tables in database and in user interface
+     */
+    protected $migration_date = '2019-03-26 20:10:40';
+
+    /*
+     * Template name
+     */
+    protected $name = 'Zoznam domén';
+
+    protected $publishable = false;
+
+    /*
+     * Automatic form and database generation
+     * @name - field name
+     * @placeholder - field placeholder
+     * @type - field type | string/text/editor/select/integer/decimal/file/password/date/datetime/time/checkbox/radio
+     * ... other validation methods from laravel
+     */
+    protected $fields = [
+        'name' => 'name:Názov webu|required|max:30',
+    ];
+
+}
